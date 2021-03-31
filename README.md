58a1d525aeba23bc7e25fe0606024876676af1f0
Erik Flores <erikfloresq@icloud.com>
2021-03-31 06:10:02 -0300

    Update docs

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
 # RickAndMortyAPI
 
-A description of this package.
+Swift wrapper for [RickandMortyAPI](https://rickandmortyapi.com)
+I coded this wrapper with combine
+
+## Quick look of methods
+
+### Character
+
+`getCharacter() -> AnyPublisher<ResponseAPI<Character>, Error>`
+
+`getCharacter(id: String) -> AnyPublisher<Character, Error>`
+
+### Episode
+
+`getEpisode() -> AnyPublisher<ResponseAPI<Episode>, Error>`
+
+`getEpisode(id: String) -> AnyPublisher<Episode, Error>`
+
+### Location
+
+`getLocation() -> AnyPublisher<ResponseAPI<Location>, Error>`
+
+
+`getLocation(id: String) -> AnyPublisher<Location, Error>`
+
+
+## Example
+
+
+```
+class ViewModel: ObservableObject {
+    let rickAndMortyAPI = RickAndMortyAPI()
+    @Published var characters = [Character]()
+    var cancellable = Set<AnyCancellable>()
+
+    func getCharacter() {
+        rickAndMortyAPI
+            .getCharacter()
+            .map { $0.results }
+            .sink { (completion) in
+                print("")
+            } receiveValue: { (characters) in
+                self.characters = characters
+            }.store(in: &cancellable)
+    }
+}
+```
+
+
+
+
+
--- Sources/RickAndMortyAPI/RickAndMortyAPI.swift
+++ Sources/RickAndMortyAPI/RickAndMortyAPI.swift
@@ -2,31 +2,38 @@ import Foundation
 import Combine
 
 @available(OSX 10.15, iOS 13.0, *)
+/// Provide characters, locations and episodes of RickAndMortyAPI
 public struct RickAndMortyAPI {
     private let networking = Networking()
 
     public init() {}
 
+    /// Get the first 20 Characters
     public func getCharacter() -> AnyPublisher<ResponseAPI<Character>, Error> {
         networking.getData(from: API.Endpoint.character("").url)
     }
 
+    /// Get a Character with id
     public func getCharacter(id: String) -> AnyPublisher<Character, Error> {
         networking.getData(from: API.Endpoint.character(id).url)
     }
 
+    /// Get the first 20 Episodes
     public func getEpisode() -> AnyPublisher<ResponseAPI<Episode>, Error> {
         networking.getData(from: API.Endpoint.episode("").url)
     }
 
+    /// Get a Episodes with id
     public func getEpisode(id: String) -> AnyPublisher<Episode, Error> {
         networking.getData(from: API.Endpoint.episode(id).url)
     }
 
+    /// Get the first 20 Locations
     public func getLocation() -> AnyPublisher<ResponseAPI<Location>, Error> {
         networking.getData(from: API.Endpoint.location("").url)
     }
 
+    /// Get a Location with id
     public func getLocation(id: String) -> AnyPublisher<Location, Error> {
         networking.getData(from: API.Endpoint.location(id).url)
     }
